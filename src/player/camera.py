061a8ca5552fe091667b1d219ab8b520e64ab6ef
PatriotRossii <patriotrossii2019@mail.ru>
2021-01-04 18:32:03 +0500

    Implement camera class

---
+++ src/player/camera.py
@@ -0,0 +1,18 @@
+from src.settings.settings import settings
+
+
+class Camera:
+    def __init__(self):
+        self.height = settings["height"]
+        self.width = settings["width"]
+
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - self.width // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - self.height // 2)
